3e5cd5f72db4fdf1ac6dfe3512ae439f59e78b69
Andreas Nygard <andreas.nygard@gmail.com>
2024-07-05 15:40:43 +1000

    package: UI kitten

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { Stack } from "expo-router";
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </ApplicationProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Layout, Text } from '@ui-kitten/components';
+
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <HomeScreen />
   );
 }
